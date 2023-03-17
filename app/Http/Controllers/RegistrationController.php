1cf9e1ba603b855d46e8f54cb4d109885dd597df
ash-tkhr <aliter.showeach@gmail.com>
2023-03-18 00:01:27 +0900

    bookmark

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -124,21 +124,21 @@ public function store(Request $request)
     public function bookmark(Request $request) {
 
         $user_id = Auth::user()->id; //1.ログインユーザーのid取得
-        $review_id = $request->review_id; //2.投稿idの取得
-        $already_liked = Like::where('user_id', $user_id)->where('review_id', $review_id)->first(); //3.
+        $article_id = $request->article_id; //2.投稿idの取得
+        $already_bookmarked = Bookmark::where('user_id', $user_id)->where('article_id', $article_id)->first(); //3.
     
-        if (!$already_liked) { //もしこのユーザーがこの投稿にまだいいねしてなかったら
-            $like = new Like; //4.Likeクラスのインスタンスを作成
-            $like->review_id = $review_id; //Likeインスタンスにreview_id,user_idをセット
-            $like->user_id = $user_id;
-            $like->save();
+        if (!$already_bookmarked) { //もしこのユーザーがこの投稿にまだいいねしてなかったら
+            $bookmark = new Bookmark; //4.bookmarkクラスのインスタンスを作成
+            $bookmark->article_id = $article_id; //bookmarkインスタンスにarticle_id,user_idをセット
+            $bookmark->user_id = $user_id;
+            $bookmark->save();
         } else { //もしこのユーザーがこの投稿に既にいいねしてたらdelete
-            Like::where('review_id', $review_id)->where('user_id', $user_id)->delete();
+            bookmark::where('article_id', $article_id)->where('user_id', $user_id)->delete();
         }
         //5.この投稿の最新の総いいね数を取得
-        $review_likes_count = Review::withCount('likes')->findOrFail($review_id)->likes_count;
+        $article_bookmarks_count = Article::withCount('bookmarks')->findOrFail($article_id)->bookmarks_count;
         $param = [
-            'review_likes_count' => $review_likes_count,
+            'article_bookmarks_count' => $article_bookmarks_count,
         ];
         return response()->json($param); //6.JSONデータをjQueryに返す
 
